6317d6be420c0081153417a8389a247bb4e01ff5
Stefan Drobnjak <drobnjak.stefan18@gmail.com>
2019-11-22 16:23:58 +0100

    Primenjivanje PSR2 Standarda nad FrontEnd klasom

--- app/Http/Controllers/FrontEnd.php
+++ app/Http/Controllers/FrontEnd.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-
 use App\Model\Comments;
 use App\Model\Posts;
 use App\Model\Users;
@@ -35,19 +34,19 @@ class FrontEnd extends BaseContoller
 
         $this->data['random_video'] = $random_video;
 
-        return view("pages.home",$this->data);
+        return view("pages.home", $this->data);
     }
 
 
-    public function single($id,$user_id = null){
+    public function single($id, $user_id = null)
+    {
 
         $visited = new Posts();
 
 
-        try{
-            $visited->VisitedPost($id,$user_id);
-        }catch (\Exception $e){
-
+        try {
+            $visited->VisitedPost($id, $user_id);
+        } catch (\Exception $e) {
             \Log::critical('Visiting post counter failed error'.$e->getMessage());
         }
 
@@ -81,17 +80,18 @@ class FrontEnd extends BaseContoller
         $this->data['post_game'] = $posts->getAllByPost(2);
 
 
-        return view('pages.single_post',$this->data);
+        return view('pages.single_post', $this->data);
     }
 
 
 
-    public function about(){
-
-        return view('pages.about',$this->data);
+    public function about()
+    {
+        return view('pages.about', $this->data);
     }
 
-    public function search(Request $request){
+    public function search(Request $request)
+    {
 
         $request->validate([
             'search_value' => 'required|max:140'
@@ -100,25 +100,20 @@ class FrontEnd extends BaseContoller
         $posts = new Posts();
 
 
-        try{
-
+        try {
             $search = $posts->LikePosts($request->search_value);
-        } catch(\Exception $e) {
-
+        } catch (\Exception $e) {
             \Log::critical('Search failed error'.$e->getMessage());
         }
 
 
 
-        if($search->isEmpty()){
+        if ($search->isEmpty()) {
                     $this->data['search_not_found'] = "No Resaults";
-                    return view('pages.search',$this->data);
-        }else {
-
-            $this->data['search_found'] = $search;
-                    return view('pages.search',$this->data);
+                    return view('pages.search', $this->data);
+        } else {
+                    $this->data['search_found'] = $search;
+                    return view('pages.search', $this->data);
         }
-
     }
-
 }
